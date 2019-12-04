7fbd00f5a3909b8ddad39dfb9d5bf256f64368b9
Colin McKibben <colin.mckibben@fireeye.com>
2019-12-04 17:17:46 -0500

    Added secrets for admin user and email password.

--- app.js
+++ app.js
@@ -17,73 +17,55 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-
-// Use this code snippet in your app.
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://aws.amazon.com/developers/getting-started/nodejs/
-
 // Load the AWS SDK
 var AWS = require('aws-sdk'),
-  region = process.env.AWS_REGION,
-  secretName = process.env.AWS_SM_DATABASE,
-  secret,
-  decodedBinarySecret;
+  region = process.env.AWS_REGION;
 
 // Create a Secrets Manager client
 var client = new AWS.SecretsManager({
   region: region
 });
 
-// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-// We rethrow the exception by default.
-
-client.getSecretValue({ SecretId: secretName }, function (err, data) {
+// Load the credentials for the initial admin user of the registration portal
+client.getSecretValue({ SecretId: process.env.AWS_SM_ADMIN_USER }, function (err, data) {
   if (err) {
-    if (err.code === 'DecryptionFailureException')
-      // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-      // Deal with the exception here, and/or rethrow at your discretion.
-      throw err;
-    else if (err.code === 'InternalServiceErrorException')
-      // An error occurred on the server side.
-      // Deal with the exception here, and/or rethrow at your discretion.
-      throw err;
-    else if (err.code === 'InvalidParameterException')
-      // You provided an invalid value for a parameter.
-      // Deal with the exception here, and/or rethrow at your discretion.
-      throw err;
-    else if (err.code === 'InvalidRequestException')
-      // You provided a parameter value that is not valid for the current state of the resource.
-      // Deal with the exception here, and/or rethrow at your discretion.
-      throw err;
-    else if (err.code === 'ResourceNotFoundException')
-      // We can't find the resource that you asked for.
-      // Deal with the exception here, and/or rethrow at your discretion.
-      throw err;
-    else
-      throw err;
+    throw err;
   }
   else {
-    // Decrypts secret using the associated KMS CMK.
-    // Depending on whether the secret is a string or binary, one of these fields will be populated.
-    if ('SecretString' in data) {
-      secret = JSON.parse(data.SecretString);
-    } else {
-      let buff = new Buffer(data.SecretBinary, 'base64');
-      decodedBinarySecret = buff.toString('ascii');
-    }
+    const secret = JSON.parse(data.SecretString);
+    process.env['ADMIN_EMAIL'] = secret.email;
+    process.env['ADMIN_PASS'] = secret.password;
   }
+});
 
-  // Your code goes here. 
-  // Connect to mongodb
-  const database = `mongodb://${secret.username}:${secret.password}@${process.env.DB_HOST}:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0`
-  mongoose.connect(database, {
-    sslCA: certFileBuf,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-    .then(() => console.log('Connection to DB successful'))
-    .catch((err) => console.error(err, 'Error'));
+// Load the password for the email account that sends out verification emails
+client.getSecretValue({ SecretId: process.env.AWS_SM_HACKATHON_EMAIL_PASSWORD }, function (err, data) {
+  if (err) {
+    throw err;
+  }
+  else {
+    const secret = JSON.parse(data.SecretString);
+    process.env['EMAIL_PASS'] = secret.password;
+  }
+});
+
+// Load the mongo DB credentials and connect to the database
+client.getSecretValue({ SecretId: process.env.AWS_SM_DATABASE_CREDENTIALS }, function (err, data) {
+  if (err) {
+    throw err;
+  }
+  else {
+    const secret = JSON.parse(data.SecretString);
+    // Connect to mongodb
+    const database = `mongodb://${secret.username}:${secret.password}@${process.env.DB_HOST}:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0`
+    mongoose.connect(database, {
+      sslCA: certFileBuf,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+      .then(() => console.log('Connection to DB successful'))
+      .catch((err) => console.error(err, 'Error'));
+  }
 });
 
 app.use(morgan('dev'));
